8bafac75c9bfad76e9503293cc2160a4bbd1d561
Vincent <vinceny.fe@gmail.com>
2023-05-25 11:13:50 +0700

    add some projects

--- common/data.ts
+++ common/data.ts
@@ -5,7 +5,10 @@ interface Category {
 export const categories: Record<string, Category> = {
     paas: {name: "Platform-as-a-Service (PaaS)"},
     iaas: {name: "Infrastructure-as-a-Service (IaaS)",},
-    notifications: {name: "Notifications"}
+    notifications: {name: "Notifications service"},
+    email: {name: "Email service"},
+    auth: {name: "Authentication service"},
+    "secrets-management": {name: "Secrets management service"}
 }
 
 interface Project {
@@ -17,15 +20,9 @@ interface Project {
 }
 
 export const projects: Project[] = [
-    {id: "vercel", name: "Vercel", description: "Build and ship full-stack applications globally, to the edge.", categoryIds: ["paas", "iaas"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
-    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"]},
+    {id: "vercel", name: "Vercel", description: "Build and ship full-stack applications globally, to the edge.", categoryIds: ["paas", "iaas"], website: "https://vercel.com"},
+    {id: "knock", name: "Knock.io", description: "Notification infrastructure that scales", categoryIds: ["paas", "notifications"], website: "https://knock.io"},
+    {id: "resend", name: "Resend", description: "Developer-friendly platform and tools that allows devs to send transactional emails at scale", categoryIds: ["paas", "email"], website: "https://resend.com"},
+    {id: "clerk", name: "Clerk", description: "Authentication-as-a-service for developers", categoryIds: ["auth"], website: "https://clerk.dev"},
+    {id: "infisical", name: "Infisical", description: "Open-source secrets management service for developers and teams", categoryIds: ["secrets-management"], website: "https://infisical.io"},
 ]
