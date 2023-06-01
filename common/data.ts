df2436332d6879d71e65e41eb356f8847010d7a1
Vincent <vinceny.fe@gmail.com>
2023-06-01 20:29:45 +0700

    add 10 more project

--- common/data.ts
+++ common/data.ts
@@ -9,6 +9,15 @@ export const categories: Record<string, Category> = {
   email: { name: "Email service" },
   auth: { name: "Authentication service" },
   "secrets-management": { name: "Secrets management service" },
+  "hosting": { name: "Web app hosting" },
+  database: { name: "Database-as-a-Service" },
+  api: { name: "API-as-a-Service" },
+  storage: {name: "Storage system"},
+  cron: {name: "Job and Cron scheduling"},
+  serverless: {name:"Serverless"},
+  oss: {name: "Open-Source Software"},
+  selfhosted: {name: "Self-hosted"},
+  cloud: {name: "Cloud"},
 };
 
 export interface Project {
@@ -25,8 +34,8 @@ export const projects: Project[] = [
     id: "vercel",
     name: "Vercel",
     description:
-      "Build and ship full-stack applications globally, to the edge.",
-    categoryIds: ["paas", "iaas"],
+      "Build and ship full-stack web applications globally, to the edge.",
+    categoryIds: ["paas", "hosting", "cron"],
     website: "https://vercel.com",
     imgPath: "/vercel.svg",
   },
@@ -42,7 +51,7 @@ export const projects: Project[] = [
     id: "novu",
     name: "Novu",
     description: "The open-source notification infrastructure for developers",
-    categoryIds: ["paas", "notifications"],
+    categoryIds: ["paas", "notifications", "oss"],
     website: "https://novu.co",
     imgPath: "/novu.svg"
   },
@@ -68,8 +77,100 @@ export const projects: Project[] = [
     name: "Infisical",
     description:
       "Open-source secrets management service for developers and teams",
-    categoryIds: ["secrets-management"],
+    categoryIds: ["secrets-management", "oss"],
     website: "https://infisical.com",
     imgPath: "/infisical.jpeg",
   },
+  {
+    id: "railway",
+    name: "Railway",
+    description:
+      "Made for any language, for projects big and small. Railway is the cloud that takes the complexity out of shipping software and web apps.",
+    categoryIds: ["paas", "hosting"],
+    website: "https://railway.app",
+    imgPath: "/railway.png",
+  },
+  {
+    id: "koyeb",
+    name: "Koyeb",
+    description:
+      "Koyeb is a developer-friendly serverless platform to deploy web apps globally. No-ops, servers, or infrastructure management.",
+    categoryIds: ["paas", "hosting"],
+    website: "https://www.koyeb.com",
+    imgPath: "/koyeb.png",
+  },
+  {
+    id: "render",
+    name: "Render",
+    description:
+      "Render is a unified cloud to build and run all your web apps and websites with free TLS certificates, a global CDN, DDoS protection, private networks, and auto deploys from Git.",
+    categoryIds: ["paas", "hosting", "serverless", "cloud"],
+    website: "https://render.com/",
+    imgPath: "/render.png",
+  },
+  {
+    id: "appwrite",
+    name: "Appwrite",
+    description:
+      `
+Appwrite is a backend platform for developing Web, Mobile, and Flutter applications. Built with the open source community and optimized for developer experience in the coding languages you love.
+`,
+    categoryIds: ["paas", "database", "oss", "serverless", "auth", "storage", "cloud", "selfhosted"],
+    website: "https://appwrite.io",
+    imgPath: "/appwrite.svg",
+  },
+  {
+    id: "supabase",
+    name: "Supabase",
+    description:
+      "Supabase is an open source Firebase alternative for building secure and performant PostgreSQL backends with minimal configuration.",
+    categoryIds: ["paas", "auth", "database", "storage", "serverless", "cloud", "selfhosted", "oss"],
+    website: "https://supabase.com/",
+    imgPath: "/supabase.svg",
+  },
+  {
+    id: "convex",
+    name: "Convex",
+    description:
+      `
+Convex is the backend application platform with everything you need to build your product.
+
+Convex functions, a database, file storage, scheduling, search, and realtime updates fit together seamlessly and are accessible from client libraries for your favorite environment.
+
+Stay in your development flow, and stop stitching things together.
+      `,
+    categoryIds: ["paas", "database", "storage", "cron", "serverless", "cloud"],
+    website: "https://convex.dev",
+    imgPath: "/convex.png",
+  },
+  {
+    id: "documenso",
+    name: "Documenso",
+    description:
+      `
+Beautiful, open document signing tools you can build upon.
+      `,
+    categoryIds: ["oss", "selfhosted"],
+    website: "https://documenso.com",
+    imgPath: "/documenso.png",
+  },
+  {
+    id: "dub",
+    name: "Dub.sh",
+    description:
+      `
+Open-sourced link management for modern marketing teams. Bit.ly alternative.
+      `,
+    categoryIds: ["oss", "selfhosted", "cloud"],
+    website: "https://dub.sh",
+    imgPath: "/dub.jpeg",
+  },
+  {
+    id: "tinybird",
+    name: "Tinybird",
+    description: "Tinybird is the modern real-time data platform for developers and data teams. Ingest data, query it with SQL, and instantly publish the results as high-concurrency, low-latency APIs.",
+    categoryIds: ["api", "iaas", "paas", "cloud"],
+    website: "https://tinybird.com",
+    imgPath: "/tinybird.jpeg",
+  }
 ];
